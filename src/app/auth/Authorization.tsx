d0ec3e3c3ea5c4c34c7ccf18ecc4f6082598d39c
Ninefinger <filip.konig@vacuumlabs.com>
2020-03-26 22:34:08 +0100

    feat(Auth & Login): JWT expiration handling & Login errors
    Add JWT expiration handling
    Add Invalid credentials error handling for Login
    Add session expired alert to login

--- src/app/auth/Authorization.tsx
+++ src/app/auth/Authorization.tsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect, useContext } from 'react';
 import AppContext from 'common/AppContext';
 import { useHistory, useLocation } from 'react-router-dom';
 import { matchRoutes } from 'react-router-config';
-import { isAuthorized } from './jwtService/jwtService2';
+import {
+  isLoggedIn,
+  getTokens,
+  getTokenLifetime,
+} from './jwtService/jwtService2';
+
+const TRASHHOLD_OFFSET = 2000;
 
 type Props = {
   children: React.ReactNode;
@@ -11,26 +17,57 @@ type Props = {
 const Authorization: React.FC<Props> = ({ children }) => {
   const { routes } = useContext(AppContext);
   const [authorized, setAuthorized] = useState<boolean>(true);
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const history = useHistory();
   const { pathname } = useLocation();
 
-  useEffect(() => {
-    if (!isAuthorized()) {
+  const checkAuth = (expired: boolean) => {
+    if (!isLoggedIn()) {
+      setLoggedIn(false);
       const isPublic =
         matchRoutes(routes, pathname)?.[0]?.route?.settings?.isPublic || false;
       if (!isPublic) {
         setAuthorized(false);
-        redirectToLogin();
+        redirectToLogin(expired);
       } else {
         setAuthorized(true);
       }
+    } else {
+      setLoggedIn(true);
     }
-  }, [pathname]);
+  };
 
-  const redirectToLogin = () => {
+  // Schedule JWT check based on its expiration
+  const setJWTCheck = () => {
+    console.log('setJWTCheck');
+    if (loggedIn) {
+      console.log('setJWTCheck - loggedIn');
+      const tokens = getTokens();
+      if (tokens) {
+        const tokenLifetimeMS = getTokenLifetime(tokens.access_token) * 1000;
+        setTimeout(() => {
+          if (isLoggedIn()) {
+            console.log('still logged in');
+            setJWTCheck();
+          } else {
+            console.log('logout');
+            checkAuth(true);
+          }
+        }, tokenLifetimeMS + TRASHHOLD_OFFSET);
+      }
+    }
+  };
+
+  // Check if User is authorized, every time when pathname changes
+  useEffect(() => checkAuth(false), [pathname]);
+
+  // Schedule JWT check every time when user logs in
+  useEffect(setJWTCheck, [loggedIn]);
+
+  const redirectToLogin = (expired: boolean) => {
     history.push({
       pathname: '/login',
-      state: { redirectUrl: pathname },
+      state: { redirectUrl: pathname, sessionExpired: expired },
     });
   };
   return authorized ? <>{children}</> : null;
--- src/app/auth/jwtService/jwtService2.ts
+++ src/app/auth/jwtService/jwtService2.ts
@@ -9,29 +9,36 @@ type TokensType = {
 // module for saving tokens to local storage
 const TOKEN_KEY = 'xyz';
 // tokens = { accessToken: "xyz", refreshToken: "abc" }
-export const saveTokens = (tokens: TokensType) => {
+export const saveTokens = (tokens: TokensType): void => {
   localStorage.setItem(TOKEN_KEY, JSON.stringify(tokens));
 };
 
-const isAuthTokenValid = (token: string) => {
+export const getTokenLifetime = (token: string): number => {
   if (!token) {
-    return false;
+    return 0;
   }
   const decoded = jwtDecode<any>(token);
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     console.warn('access token expired');
+    return 0;
+  }
+  return decoded.exp - currentTime;
+};
+
+const isAuthTokenValid = (token: string): boolean => {
+  if (!token) {
     return false;
   }
 
-  return true;
+  return getTokenLifetime(token) > 0;
 };
 
-export const deleteTokens = () => {
+export const deleteTokens = (): void => {
   localStorage.removeItem(TOKEN_KEY);
 };
 
-export const getTokens = () => {
+export const getTokens = (): TokensType | null => {
   const tokensString = localStorage.getItem(TOKEN_KEY);
   const tokens = tokensString ? JSON.parse(tokensString) : null;
   if (!isAuthTokenValid(tokens?.access_token)) {
@@ -41,4 +48,4 @@ export const getTokens = () => {
   return tokens;
 };
 
-export const isAuthorized = () => !!getTokens();
+export const isLoggedIn = () => !!getTokens();
