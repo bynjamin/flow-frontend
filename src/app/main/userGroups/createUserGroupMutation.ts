a7b3c3c7a5df8e482a8691afb7a7d82b57dc1e14
Ninefinger <filip.konig@vacuumlabs.com>
2020-03-31 15:19:04 +0200

    chore(Schema): Update mutation names according to new schema

--- src/app/main/userGroups/createUserGroupMutation.ts
+++ src/app/main/userGroups/createUserGroupMutation.ts
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export const CREATE_USER_GROUP = gql`
   mutation CreateUserGroup($name: String!, $description: String!) {
-    CreateUserGroup(name: $name, description: $description) {
+    createUserGroup(name: $name, description: $description) {
       id
     }
   }
