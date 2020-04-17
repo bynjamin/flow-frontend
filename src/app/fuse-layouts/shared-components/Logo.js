235b92504e9b0bb8570b8ab57dc470bfa6426f20
Ninefinger <filip.konig@vacuumlabs.com>
2020-04-17 20:36:39 +0200

    feat(Fuse to Flowato): Change navigation header and html title

--- src/app/fuse-layouts/shared-components/Logo.js
+++ src/app/fuse-layouts/shared-components/Logo.js
@@ -1,5 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import StarIcon from '@material-ui/icons/Star';
 import clsx from 'clsx';
 import React from 'react';
 
@@ -13,17 +14,20 @@ const useStyles = makeStyles(theme => ({
 				easing: theme.transitions.easing.easeInOut
 			})
 		},
-		'& .react-badge, & .logo-text': {
+		'& .subscription-badge, & .logo-text': {
 			transition: theme.transitions.create('opacity', {
 				duration: theme.transitions.duration.shortest,
 				easing: theme.transitions.easing.easeInOut
 			})
 		}
 	},
-	reactBadge: {
+	subscriptionBadge: {
 		backgroundColor: '#121212',
 		color: '#61DAFB'
-	}
+  },
+  subscriptionBadgeIcon: {
+    fontSize: 12,
+  }
 }));
 
 function Logo() {
@@ -33,16 +37,11 @@ function Logo() {
 		<div className={clsx(classes.root, 'flex items-center')}>
 			<img className="logo-icon" src="assets/images/logos/fuse.svg" alt="logo" />
 			<Typography className="text-16 mx-12 font-light logo-text" color="inherit">
-				FUSE
+				Flowato
 			</Typography>
-			<div className={clsx(classes.reactBadge, 'react-badge flex items-center py-4 px-8 rounded')}>
-				<img
-					className="react-logo"
-					src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
-					alt="react"
-					width="16"
-				/>
-				<span className="react-text text-12 mx-4">React</span>
+			<div className={clsx(classes.subscriptionBadge, 'subscription-badge flex items-center py-4 px-8 rounded')}>
+				<StarIcon className={classes.subscriptionBadgeIcon} />
+				<span className="react-text text-12 mx-4">Premium</span>
 			</div>
 		</div>
 	);
